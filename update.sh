f0c36e7d6d9bc8f389f4de3b671db51f72a51844
Frost Ming <me@frostming.com>
2024-10-08 12:29:24 +0800

    chore: enable auto deploy
    Signed-off-by: Frost Ming <me@frostming.com>

--- update.sh
+++ update.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -ex
 
+cd "$(dirname "$0")"
+
 git fetch origin
 git reset --hard origin/main
 
-pdm sync
+~/.local/bin/pdm sync
 
 source .venv/bin/activate
 export DJANGO_SETTINGS_MODULE=pycon.settings.production
 python manage.py migrate --noinput
+python manage.py collectstatic --noinput
 sudo systemctl restart pycon.service
