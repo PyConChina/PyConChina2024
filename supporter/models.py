6ce32ebca19439da182b6e2cac68e3fe328dc2ce
Frost Ming <me@frostming.com>
2024-10-09 17:45:24 +0800

    feat: load nav item from menu
    Signed-off-by: Frost Ming <me@frostming.com>

--- supporter/models.py
+++ supporter/models.py
@@ -1,21 +1,56 @@
-from wagtail.admin.panels import FieldPanel
-from wagtail.fields import StreamField
-from wagtail.models import Page
-
-from supporter.blocks import SupporterStreamBlock
+from django import forms
+from django.db import models
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+from wagtail.admin.panels import FieldPanel, InlinePanel
+from wagtail.fields import RichTextField
+from wagtail.models import Orderable, Page
+from wagtail.snippets.models import register_snippet
 
 # Create your models here.
 
 
 class SupporterPage(Page):
     parent_page_types = ["home.HomePage"]
-
-    body = StreamField(
-        SupporterStreamBlock(),
-        use_json_field=True,
-        help_text="Add supporters to the page.",
-    )
+    body = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
         FieldPanel("body"),
+        InlinePanel("supporters", label="Supporters"),
     ]
+
+
+class SupporterTier(Orderable):
+    page = ParentalKey(
+        SupporterPage, on_delete=models.CASCADE, related_name="supporters"
+    )
+    name = models.CharField(max_length=64)
+    supporters = ParentalManyToManyField("supporter.Supporter")
+
+    panels = [
+        FieldPanel("name"),
+        FieldPanel("supporters", widget=forms.CheckboxSelectMultiple),
+    ]
+
+
+@register_snippet
+class Supporter(models.Model):
+    name = models.CharField(max_length=32)
+    description = models.CharField(max_length=255, blank=True)
+    logo = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+    url = models.URLField(blank=True)
+
+    panels = [
+        FieldPanel("name"),
+        FieldPanel("description"),
+        FieldPanel("logo"),
+        FieldPanel("url"),
+    ]
+
+    def __str__(self) -> str:
+        return self.name
--- talk/models.py
+++ talk/models.py
@@ -1,7 +1,10 @@
+from django import forms
 from django.db import models
-from wagtail.admin.panels import FieldPanel, InlinePanel
+from modelcluster.fields import ParentalManyToManyField
+from wagtail.admin.panels import FieldPanel
 from wagtail.fields import RichTextField
-from wagtail.models import Orderable, Page, ParentalKey
+from wagtail.models import Page
+from wagtail.snippets.models import register_snippet
 
 
 class TalkType(models.TextChoices):
@@ -26,20 +29,31 @@ class TalkPage(Page):
     type = models.CharField(
         max_length=32, choices=TalkType.choices, default=TalkType.KEYNOTE
     )
+    authors = ParentalManyToManyField("talk.Author")
 
     content_panels = Page.content_panels + [
-        InlinePanel("authors", heading="Authors", label="Authors"),
+        FieldPanel("authors", widget=forms.CheckboxSelectMultiple),
         FieldPanel("abstract"),
         FieldPanel("body"),
         FieldPanel("type"),
     ]
 
 
-class Author(Orderable):
-    page = ParentalKey(TalkPage, on_delete=models.CASCADE, related_name="authors")
+@register_snippet
+class Author(models.Model):
     name = models.CharField(max_length=255, help_text="Name of the author")
     avatar = models.ForeignKey(
         "wagtailimages.Image", on_delete=models.CASCADE, related_name="+"
     )
     bio = models.CharField(max_length=255, blank=True, help_text="Bio of the author")
     introduction = RichTextField(blank=True, help_text="Introduction of the author")
+
+    panels = [
+        FieldPanel("name"),
+        FieldPanel("avatar"),
+        FieldPanel("bio"),
+        FieldPanel("introduction"),
+    ]
+
+    def __str__(self):
+        return self.name
