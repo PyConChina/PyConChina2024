b87a33d4a3e3b5465e87f39bc658be0b02c7f32c
Frost Ming <me@frostming.com>
2024-11-07 12:53:19 +0800

    fix: home page and 404
    Signed-off-by: Frost Ming <me@frostming.com>

--- home/models.py
+++ home/models.py
@@ -1,18 +1,24 @@
 from django.db import models
 from wagtail.admin.panels import FieldPanel, InlinePanel
-from wagtail.fields import RichTextField
+from wagtail.fields import RichTextField, StreamField
+from wagtail import blocks
 from wagtail.models import Orderable, Page, ParentalKey
+
+
+class ContentBlock(blocks.StructBlock):
+    title = blocks.CharBlock(required=True, form_classname="title")
+    paragraph = blocks.RichTextBlock()
 
 
 class HomePage(Page):
     venue = models.CharField(max_length=255, blank=True, null=True)
     date = models.DateField(blank=True, null=True)
-    about = RichTextField(blank=True)
+    body = StreamField([('paragraph', ContentBlock())])
 
     content_panels = Page.content_panels + [
         FieldPanel("venue"),
         FieldPanel("date"),
-        FieldPanel("about"),
+        FieldPanel("body"),
         InlinePanel("links", heading="Related Links", label="Related Links"),
     ]
 
