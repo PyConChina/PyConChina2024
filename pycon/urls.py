dc05acc12eb5a63046d726f5d3085838ac315383
Frost Ming <me@frostming.com>
2024-11-25 12:19:06 +0800

    fix: documents url
    Signed-off-by: Frost Ming <me@frostming.com>

--- pycon/urls.py
+++ pycon/urls.py
@@ -10,7 +10,6 @@ from search import views as search_views
 urlpatterns = [
     path("django-admin/", admin.site.urls),
     path("admin/", include(wagtailadmin_urls)),
-    path("documents/", include(wagtaildocs_urls)),
     path("search/", search_views.search, name="search"),
 ]
 
@@ -27,6 +26,7 @@ urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
+    path("2024/documents/", include(wagtaildocs_urls)),
     path("2024/", include(wagtail_urls)),
     # Alternatively, if you want Wagtail pages to be served from a subpath
     # of your site, rather than the site root:
