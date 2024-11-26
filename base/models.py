0a1b83afd44c60889bbb2066f22ae7d510d31a68
Frost Ming <me@frostming.com>
2024-11-26 17:30:24 +0800

    feat: social links
    Signed-off-by: Frost Ming <me@frostming.com>

--- base/models.py
+++ base/models.py
@@ -1,27 +1,28 @@
 from django.db import models
-from wagtail.admin.panels import (
-    FieldPanel,
-    MultiFieldPanel,
-)
+from modelcluster.models import ClusterableModel
+from wagtail.admin.panels import FieldPanel, InlinePanel
 from wagtail.contrib.settings.models import (
     BaseGenericSetting,
     register_setting,
 )
+from wagtail.models import Orderable, ParentalKey
 
 
 @register_setting
-class NavigationSettings(BaseGenericSetting):
-    twitter_url = models.URLField(verbose_name="Twitter URL", blank=True)
-    github_url = models.URLField(verbose_name="GitHub URL", blank=True)
-    home_url = models.URLField(verbose_name="Home URL", blank=True)
+class NavigationSettings(BaseGenericSetting, ClusterableModel):
+    panels = [
+        InlinePanel("social_links", label="Social Links"),
+    ]
+
+
+class SocialLink(Orderable):
+    page = ParentalKey("base.NavigationSettings", related_name="social_links")
+    name = models.CharField(max_length=32)
+    url = models.URLField()
+    icon = models.CharField(max_length=32, blank=True)
 
     panels = [
-        MultiFieldPanel(
-            [
-                FieldPanel("twitter_url"),
-                FieldPanel("github_url"),
-                FieldPanel("home_url"),
-            ],
-            "Social settings",
-        )
+        FieldPanel("name"),
+        FieldPanel("url"),
+        FieldPanel("icon"),
     ]
